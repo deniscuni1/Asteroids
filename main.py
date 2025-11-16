4d2bd61c7197a906df65e4f16451cdede7227e8c
deniscuni01 <denisckck25@gmail.com>
2025-11-16 22:41:01 +0000

    Changed main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -12,5 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 if __name__ == "__main__":
     main()
